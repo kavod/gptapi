84705284835c11c41293ad9e62affa6adb138c03
kavod <brice.grichy@gmail.com>
2024-05-15 22:06:39 +0200

    test apikey

---
+++ test_apikey.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI()
+# defaults to getting the key using os.environ.get("OPENAI_API_KEY")
+# if you saved the key under a different environment variable name, you can do something like:
+# client = OpenAI(
+#   api_key=os.environ.get("CUSTOM_ENV_NAME"),
+# )
